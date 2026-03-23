33ab062c00f46f6a58f50eaf95084454da098cda
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-23 21:34:15 +0100

    initialize backend

---
+++ roomnexus-backend/src/main/java/com/roomnexus/backend/RoomnexusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roomnexus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomnexusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomnexusBackendApplication.class, args);
+	}
+
+}
---
+++ roomnexus-backend/src/test/java/com/roomnexus/backend/RoomnexusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomnexus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomnexusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
