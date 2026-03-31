2d2030d873c9608a6af67fc594a1cdfc02640942
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-31 19:38:14 +0200

    add controllers files

---
+++ roomnexus-backend/src/main/java/com/roomnexus/backend/controller/BookingController.java
@@ -0,0 +1,11 @@
+package com.roomnexus.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/bookings")
+@RequiredArgsConstructor
+public class BookingController {
+}
