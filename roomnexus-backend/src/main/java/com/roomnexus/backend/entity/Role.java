7dbc0a91d9cb9f4640997ee3e2e4cf17b3ba4357
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-31 18:36:48 +0200

    add entity Role.java and UserProfile.java

---
+++ roomnexus-backend/src/main/java/com/roomnexus/backend/entity/Role.java
@@ -0,0 +1,8 @@
+package com.roomnexus.backend.entity;
+
+public enum Role {
+    SUPER_ADMIN,
+    ADMIN,
+    USER
+
+}
---
+++ roomnexus-backend/src/main/java/com/roomnexus/backend/entity/UserProfile.java
@@ -0,0 +1,38 @@
+package com.roomnexus.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user_profile")
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotBlank(message = "The user's keycloak id is required")
+    private String keycloakUserId;
+
+    @NotBlank(message = "The user's firstname is required")
+    private String firstName;
+
+    @NotBlank(message = "The user's lastname is required")
+    private String lastName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Company company;
+
+    @NotBlank(message = "The user's role is required")
+    private Role role;
+}
