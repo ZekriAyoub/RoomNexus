84fe4905d80725a2c6498135b92d2b495ee011bf
ZekriAyoub <ayoub.zekri.3@icloud.com>
2026-03-31 18:35:59 +0200

    add entity Company.java

---
+++ roomnexus-backend/src/main/java/com/roomnexus/backend/entity/Company.java
@@ -0,0 +1,30 @@
+package com.roomnexus.backend.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "company")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotBlank(message = "The company's name is required")
+    private String name;
+
+    @NotBlank(message = "The company's keycloak group id is required")
+    private String keycloakGroupId;
+
+}
